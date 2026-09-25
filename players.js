424ee210deefd68f8437abfbea09a146bfc1d1e2
EzDevPy <samtarpro@gmail.com>
2026-09-25 19:51:56 +0300

    Update players.js by great_terku (Role: R6 Власник Сервера)

--- players.js
+++ players.js
@@ -147,12 +147,6 @@ const players = {
             "role": "Головний Суддя",
             "telegram": "t.me/Samyry228",
             "avatar": "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-E913BC846B4C63706C0E103F604EB4D9-Png/150/150/AvatarHeadshot/Webp/noFilter"
-        },
-        {
-            "username": "mummu228kuku",
-            "role": "Заступник Головного Судді",
-            "telegram": "t.me/here_everyone",
-            "avatar": "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-4D74895EEC1F4CA85C925E04EBA61D55-Png/150/150/AvatarHeadshot/Png/noFilter"
         }
     ],
     "Мерія": [],
